c1949a0b3811b268633ad6a102a0c866a2c7dafa
BobSaidHi <47067448+BobSaidHi@users.noreply.github.com>
2026-06-01 01:23:38 -0700

    Drafted part 1

--- lab4.py
+++ lab4.py
@@ -1,31 +1,37 @@
 import stat
 import dataclasses
 from agents import EntityAgent, UncertainAgent
-from model import Location,GameState, GameAction, WizardMoves, Location, Crystal, Portal, Lava, GameTransitions, Observation, EmptyTile, LocationCounts, LocationDistribution
+from model import Location, GameState, GameAction, WizardMoves, Location, \
+    Crystal, Portal, Lava, GameTransitions, Observation, EmptyTile, \
+    LocationCounts, LocationDistribution, Wall
 import random
 
 
 class MDP:
-    def __init__(self,initial_state: GameState, escape_reward:float,living_reward: float,death_reward:float, discount: float):
+    def __init__(self, initial_state: GameState, escape_reward: float,
+                 living_reward: float, death_reward: float, discount: float):
         self.game_state = initial_state
         self.living_reward = living_reward
         self.death_reward = death_reward
         self.escape_reward = escape_reward
         self.discount = discount
 
-    def reward(self,source:GameState,target:GameState, action: GameAction) -> float:
+    def reward(self, source: GameState, target: GameState,
+               action: GameAction) -> float:
         loc = target.active_entity_location
-        if isinstance(target.tile_grid[loc.row][loc.col],Lava):
+        if isinstance(target.tile_grid[loc.row][loc.col], Lava):
             return self.death_reward
         elif target.victory:
             return self.escape_reward
         else:
             return self.living_reward
 
-
-    def transition_model(self,location: Location, action: GameAction) -> LocationDistribution:
+    def transition_model(self, location: Location,
+                         action: GameAction) -> LocationDistribution:
         """
-        Transition model of the MDP, gives conditional probability distribution of result location given starting location and action choice.
+        Transition model of the MDP, gives conditional probability
+        distribution of result location given starting location and action
+        choice.
         """
         source_state = self.game_state.replace_active_entity_location(location)
         successors = GameTransitions.get_successors(source_state)
@@ -33,35 +39,41 @@ class MDP:
         successor_states = [state for _, state in successors]
 
         if action not in actions:
-            return self.transition_model(location,WizardMoves.STAY)
+            return self.transition_model(location, WizardMoves.STAY)
 
-        # Outcomes are either the desired outcome of the action, or a random other action each with 50% prob.
+        # Outcomes are either the desired outcome of the action, or a random
+        # other action each with 50% prob.
         possible_results = LocationCounts(self.game_state.grid_size)
         for i in range(len(actions)):
             if actions[i] == action:
-                for _ in range(len(actions)+1):
-                    possible_results.add_count(successor_states[i].active_entity_location)
+                for _ in range(len(actions) + 1):
+                    possible_results.add_count(
+                        successor_states[i].active_entity_location)
             else:
-                possible_results.add_count(successor_states[i].active_entity_location)
+                possible_results.add_count(
+                    successor_states[i].active_entity_location)
 
         return possible_results.normalize()
 
-    def transition_distribution(self, source: LocationDistribution, action: GameAction) -> LocationDistribution:
+    def transition_distribution(self, source: LocationDistribution,
+                                action: GameAction) -> LocationDistribution:
         """
-        Given a location distribution, calculate the new distribution that is a result of taking the given action.
+        Given a location distribution, calculate the new distribution that is
+        a result of taking the given action.
         The easiest way to do this will involve sampling.
         """
 
-        #TODO YOUR CODE HERE
+        # TODO YOUR CODE HERE
         raise NotImplementedError()
 
 
 class LocationValues:
     def __init__(self, mdp: MDP):
         self.mdp = mdp
-        self.value_grid = [[0.0 for _ in range(mdp.game_state.grid_size[1])] for _ in range(mdp.game_state.grid_size[0])]
+        self.value_grid = [[0.0 for _ in range(mdp.game_state.grid_size[1])] for
+                           _ in range(mdp.game_state.grid_size[0])]
 
-    def value_iteration(self,k):
+    def value_iteration(self, k):
         for _ in range(k):
             self.value_iteration_update()
 
@@ -70,11 +82,88 @@ class LocationValues:
         Perform one update of value iteration based off of the provided MDP.
         """
 
-        next_value_grid = [[0.0 for _ in range(self.mdp.game_state.grid_size[1])] for _ in range(self.mdp.game_state.grid_size[0])]
+        # 1. Create a new empty grid for the next iteration's values
+        next_value_grid = [
+            [0.0 for _ in range(self.mdp.game_state.grid_size[1])] for _ in
+            range(self.mdp.game_state.grid_size[0])]
 
+        # DONE: YOUR CODE HERE, CALCULATE NEXT VALUE AS A FUNCTION OF PREVIOUS
+        #  VALUE
 
-        #TODO YOUR CODE HERE, CALCULATE NEXT VALUE AS A FUNCTION OF PREVIOUS VALUE
-        raise NotImplementedError()
+        # 2. Loop through every location in the grid
+        for row in range(self.mdp.game_state.grid_size[0]):
+            for col in range(self.mdp.game_state.grid_size[1]):
+                loc = Location(row, col)
+                tile = self.mdp.game_state.tile_grid[row][col]
+
+                # 3. Check if this is a TERMINAL STATE (Lava or Portal)
+                #    Terminal states don't need additional work, their value
+                #    is the reward
+                if isinstance(tile, (Lava, Portal)):
+                    # Create a state at this location to get its reward
+                    stateAtLoc = (
+                        self.mdp.game_state.replace_active_entity_location(
+                            loc))
+                    next_state = stateAtLoc  # These don't move, just exist
+                    next_value_grid[row][col] = self.mdp.reward(stateAtLoc,
+                                                                next_state,
+                                                                None)
+                    continue
+
+                # 4. Check if this is a WALL (can't be in this state)
+                if isinstance(tile, Wall):
+                    next_value_grid[row][col] = 0.0
+                    continue
+
+                # 5. For non-terminal, non-wall locations: try each valid action
+                bestValue = float('-inf')
+
+                # Get valid actions from this location
+                source_state = (
+                    self.mdp.game_state.replace_active_entity_location(
+                    loc))
+                successors = GameTransitions.get_successors(source_state)
+                valid_actions = [action for action, _ in successors]
+
+                # Try each action
+                for action in valid_actions:
+                    # Get the probability distribution of where we'll end up
+                    outcomeDist = self.mdp.transition_model(loc, action)
+
+                    # Calculate expected value of taking this action
+                    expectedValue = 0.0
+
+                    # Sum over all possible outcome locations
+                    for outcome_loc in outcomeDist.locations():
+                        # Probability of ending up there
+                        prob = outcomeDist.probability(outcome_loc)
+
+                        # Create states to calculate reward
+                        outcomeState = (
+                            self.mdp.game_state.replace_active_entity_location(
+                                outcome_loc))
+
+                        # Immediate reward for this transition
+                        reward = self.mdp.reward(source_state, outcomeState,
+                                                 action)
+
+                        # Discounted future value (using current value_grid,
+                        # not next_value_grid!)
+                        futureValue = self.mdp.discount * \
+                                      self.value_grid[outcome_loc.row][
+                                          outcome_loc.col]
+
+                        # Add weighted contribution
+                        expectedValue += prob * (reward + futureValue)
+
+                    # Track the best action
+                    bestValue = max(bestValue, expectedValue)
+
+                # Store the best value for this location
+                next_value_grid[row][col] = bestValue
+
+        # 6. Update the current value grid with the new values
+        self.value_grid = next_value_grid
 
         return next_value_grid
 
@@ -89,36 +178,42 @@ class MDPAgent(UncertainAgent):
         self.mdp = mdp
         self.values = LocationValues(mdp)
         self.values.value_iteration(value_iteration_steps)
-        self.current_position_estimate = LocationDistribution.from_game_state_uniform(mdp.game_state)
+        self.current_position_estimate = (
+            LocationDistribution.from_game_state_uniform(
+                mdp.game_state))
 
-
-    def observation_likelihood(self, observation: Observation, loc: Location)-> float:
+    def observation_likelihood(self, observation: Observation,
+                               loc: Location) -> float:
         portal_loc = self.mdp.game_state.get_all_tile_locations(Portal)[0]
-        portal_dist = abs(loc.row-portal_loc.row) + abs(loc.col - portal_loc.col)
+        portal_dist = abs(loc.row - portal_loc.row) + abs(
+            loc.col - portal_loc.col)
 
         if abs(portal_dist - observation.approximatePortalDist) > 1:
             return 0
         else:
-            return 1.0/3.0
+            return 1.0 / 3.0
 
-    def update_prior(self,action: GameAction):
-        self.current_position_estimate = self.mdp.transition_distribution(self.current_position_estimate,action)
-
+    def update_prior(self, action: GameAction):
+        self.current_position_estimate = self.mdp.transition_distribution(
+            self.current_position_estimate, action)
 
     def update_belief(self, observation: Observation):
         """
-        Use Bayes rule to update your beliefs about the wizard location by updating self.current_position_estimate.
-        You have prior belief of your current estimate P(Loc), and the observation likelihood model (P(Obs | Loc)).
+        Use Bayes rule to update your beliefs about the wizard location by
+        updating self.current_position_estimate.
+        You have prior belief of your current estimate P(Loc), and the
+        observation likelihood model (P(Obs | Loc)).
         Use these to calculate the new belief.
         """
 
-        #We need to update our belief for all possible locations. So lets start by creating a new distribution
-        new_estimate = LocationDistribution.from_game_state_uniform(self.mdp.game_state)
+        # We need to update our belief for all possible locations. So lets
+        # start by creating a new distribution
+        new_estimate = LocationDistribution.from_game_state_uniform(
+            self.mdp.game_state)
 
-        #The new distribution should be set for each location
+        # The new distribution should be set for each location
         for loc in new_estimate.locations():
-
-            #TODO: YOUR CODE HERE
+            # TODO: YOUR CODE HERE
             raise NotImplementedError()
 
         new_estimate.renormalize()
@@ -126,26 +221,42 @@ class MDPAgent(UncertainAgent):
 
     def react(self, observation: Observation) -> GameAction:
         """
-        Our uncertain agent only has noisy observations to guess where in the dungeon it is. Use the previously implemented parts to generate an estimate for the distribution of possible locations the wizard might be at, updating every turn with a new observation, and choose the action based off of your value iteration policy based on your estimate.
+        Our uncertain agent only has noisy observations to guess where in the
+        dungeon it is. Use the previously implemented parts to generate an
+        estimate for the distribution of possible locations the wizard might
+        be at, updating every turn with a new observation, and choose the
+        action based off of your value iteration policy based on your estimate.
         """
 
-        # Use Bayes Rule to update your beliefs about where you think the wizard is based off of the observation
+        # Use Bayes Rule to update your beliefs about where you think the
+        # wizard is based off of the observation
         self.update_belief(observation)
 
-
         # Part 2: Choose the best action
-        # use your calculated value iteration map of location values alongside your estimated location to choose the best action given your uncertain state.
+        # use your calculated value iteration map of location values
+        # alongside your estimated location to choose the best action given
+        # your uncertain state.
         # There are multiple ways to do this, but some things to consider:
-            # 1. You want to select the action which will have the highest expected value, given the probability distribution of the resultant states.
-            # 2. The expected value of some quantity is just the weighted average value of that quantity in an outcome weighted by the probability of that outcome over all outcomes (and can be estimated by the average of a sufficiently big sample of outcomes sampled from the distribution)
-            # 3. You can sample locations from any distribution, and can form distributions of locations from samples
-            # 4. You can find the distribution of the results of an action for a given specific location
-            # 5. You can calculate the reward of a specific transition as a result of a specific action with a specific result
-            # 6. You have an estimate of the value of each result location
-        #TODO YOUR CODE HERE
+        # 1. You want to select the action which will have the highest
+        # expected value, given the probability distribution of the resultant
+        # states.
+        # 2. The expected value of some quantity is just the weighted average
+        # value of that quantity in an outcome weighted by the probability of
+        # that outcome over all outcomes (and can be estimated by the average
+        # of a sufficiently big sample of outcomes sampled from the
+        # distribution)
+        # 3. You can sample locations from any distribution, and can form
+        # distributions of locations from samples
+        # 4. You can find the distribution of the results of an action for a
+        # given specific location
+        # 5. You can calculate the reward of a specific transition as a
+        # result of a specific action with a specific result
+        # 6. You have an estimate of the value of each result location
+        # TODO YOUR CODE HERE
         raise NotImplementedError()
         action = WizardMoves.RIGHT
 
-        #When choosing an action, we must update our prior to account for the new distribution as a result of the action being taken
+        # When choosing an action, we must update our prior to account for
+        # the new distribution as a result of the action being taken
         self.update_prior(action)
         return action
--- run.py
+++ run.py
@@ -1,34 +1,34 @@
 from agents import (
     UncertainAgent,
 )
-from model import  Wizard
+from model import Wizard
 import pyglet
 
-from lab4 import ( MDPAgent, MDP)
-
+from lab4 import (MDPAgent, MDP)
 
 from game import MDPGame
 import argparse
 
-
 parser = argparse.ArgumentParser()
-
 
 parser.add_argument("--map", type=str, default="open", help="Map name")
 
-
 parser.add_argument("--speed", type=float, default=10, help="Moves per second")
 parser.add_argument(
-    "--value_iteration_steps", type=int, default=100, help="Number of steps to run value iteration"
+    "--value_iteration_steps", type=int, default=100,
+    help="Number of steps to run value iteration"
 )
 parser.add_argument(
-    "--escape_reward", type=float, default=100, help="Reward given to agent if it can escape through the portal"
+    "--escape_reward", type=float, default=100,
+    help="Reward given to agent if it can escape through the portal"
 )
 parser.add_argument(
-    "--living_reward", type=float, default=0, help="Reward given to agent at each step it is alive"
+    "--living_reward", type=float, default=0,
+    help="Reward given to agent at each step it is alive"
 )
 parser.add_argument(
-    "--death_reward", type=float, default=-50, help="Reward given to agent at its death"
+    "--death_reward", type=float, default=-50,
+    help="Reward given to agent at its death"
 )
 
 parser.add_argument(
@@ -40,11 +40,11 @@ parser.add_argument(
 )
 
 parser.add_argument(
-    "--no_render", action="store_true", help="Whether to not render search nodes"
+    "--no_render", action="store_true",
+    help="Whether to not render search nodes"
 )
 parser.add_argument("--debug", action="store_true", help="Enable debug output")
 args = parser.parse_args()
-
 
 if __name__ == "__main__":
     game = MDPGame(
@@ -56,14 +56,32 @@ if __name__ == "__main__":
     )
 
     mdp = MDP(initial_state=game.state,
-        escape_reward=args.escape_reward,
-        living_reward=args.living_reward,
-        death_reward=args.death_reward,
-        discount=args.discount)
+              escape_reward=args.escape_reward,
+              living_reward=args.living_reward,
+              death_reward=args.death_reward,
+              discount=args.discount)
 
     for _ in game.state.get_all_entity_locations(Wizard):
-        agent = MDPAgent(mdp=mdp,value_iteration_steps=args.value_iteration_steps)
-        game.register_entity_agent(agent,1)
+        agent = MDPAgent(mdp=mdp,
+                         value_iteration_steps=args.value_iteration_steps)
+
+        print("\n=== Value Iteration Results ===")
+        print(f"Grid Size: {mdp.game_state.grid_size}")
+        print("\nValue Grid (higher values = more valuable):")
+        for row in range(mdp.game_state.grid_size[0]):
+            print("  ".join(
+                f"{mdp.game_state.tile_grid[row][col].__class__.__name__:>2}"
+                for col in range(mdp.game_state.grid_size[1])))
+
+        print("\nTile Values:")
+        for row in range(mdp.game_state.grid_size[0]):
+            rowValues = []
+            for col in range(mdp.game_state.grid_size[1]):
+                value = agent.values.value_grid[row][col]
+                rowValues.append(f"{value:7.2f}")
+            print("  ".join(rowValues))
+
+        game.register_entity_agent(agent, 1)
 
     game.run()
 
